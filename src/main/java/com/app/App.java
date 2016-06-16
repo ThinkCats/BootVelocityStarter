a36baa4ed1436f36f0a7a50e759d709431441d5c
wanglei <wanglei@souche.com>
2016-06-16 17:50:45 +0800

    update entry

--- src/main/java/com/app/App.java
+++ src/main/java/com/app/App.java
@@ -1,15 +1,9 @@
 package com.app;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.context.annotation.ImportResource;
 
-@Configuration
-@EnableAutoConfiguration
 @ComponentScan(value = "com.busi.**")
 @SpringBootApplication
 public class App {
