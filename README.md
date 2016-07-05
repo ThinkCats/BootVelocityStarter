bc60eb93859cb58bd5f9862f3f4c63f2d4bfb9aa
wanglei <wanglei@souche.com>
2016-07-05 11:11:23 +0800

    download support

--- README.md
+++ README.md
@@ -3,13 +3,20 @@
 	2.Default User login: admin / 12345
 	3.Add custom annotation and aop process
 
+### UPDATE (201616-07-05)
+    1. Add Download
+        * Using HttpServletResponse (void method)
+        * Using Spring HttpEntity
+        * Using HttpServletResponse (return Resource)
+
 ### UPDATE
     1.Live Reload VM option:
      ```
      -javaagent:/Users/dasouche/.m2/repository/org/springframework/springloaded/1.2.3.RELEASE/springloaded-1.2.3.RELEASE.jar -noverify
 
      ```
-----
+
+### UPDATE
     1.Add a custom method annotation : @Loggable
     2.Add a custom method param annotation : @SerialNumber
 
---
+++ src/main/java/com/busi/controller/DownloadController.java
@@ -0,0 +1,86 @@
+package com.busi.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Created by wl on 16/7/5.
+ */
+@Slf4j
+@Controller
+@RequestMapping("/download")
+public class DownloadController {
+
+    private final String APPLICATION_PDF = "application/pdf";
+
+    @RequestMapping(value = "/a", method = RequestMethod.GET, produces = APPLICATION_PDF)
+    public @ResponseBody
+    void downloadA(HttpServletResponse response) throws IOException {
+        File file = getFile();
+        InputStream in = new FileInputStream(file);
+
+        response.setContentType(APPLICATION_PDF);
+        response.setHeader("Content-Disposition", "attachment; filename=" + file.getName());
+        response.setHeader("Content-Length", String.valueOf(file.length()));
+        FileCopyUtils.copy(in, response.getOutputStream());
+    }
+
+    @RequestMapping(value = "/b", method = RequestMethod.GET, produces = APPLICATION_PDF)
+    public @ResponseBody
+    HttpEntity<byte[]> downloadB() throws IOException {
+        File file = getFile();
+        byte[] document = FileCopyUtils.copyToByteArray(file);
+
+        HttpHeaders header = new HttpHeaders();
+        header.setContentType(new MediaType("application", "pdf"));
+        header.set("Content-Disposition", "attachment; filename=" + file.getName());
+        header.setContentLength(document.length);
+
+        return new HttpEntity<>(document, header);
+    }
+
+    @RequestMapping(value = "/c", method = RequestMethod.GET, produces = APPLICATION_PDF)
+    public @ResponseBody
+    Resource downloadC(HttpServletResponse response) throws FileNotFoundException {
+        File file = getFile();
+        response.setContentType(APPLICATION_PDF);
+        response.setHeader("Content-Disposition", "inline; filename=" + file.getName());
+        response.setHeader("Content-Length", String.valueOf(file.length()));
+        return new FileSystemResource(file);
+    }
+
+    private File getFile(){
+        ResourceLoader loader = new DefaultResourceLoader();
+        Resource resource = loader.getResource(ResourceLoader.CLASSPATH_URL_PREFIX + "res/pdf/template.pdf");
+        try {
+            File file = new File(resource.getURL().getPath());
+            if (!file.exists()){
+                log.error("File Not Found !!");
+                return null;
+            }
+            return file;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+}
