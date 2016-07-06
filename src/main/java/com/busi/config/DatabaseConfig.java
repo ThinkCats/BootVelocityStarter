63fbaad326093d2ee6b6cae5f04c9595c6e1045d
wanglei <wanglei@souche.com>
2016-07-06 16:13:32 +0800

    update H2 Database

--- src/main/java/com/busi/config/DatabaseConfig.java
+++ src/main/java/com/busi/config/DatabaseConfig.java
@@ -1,5 +1,6 @@
 package com.busi.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -10,6 +11,7 @@ import javax.annotation.PostConstruct;
  * Created by wl on 16/7/5.
  * For H2 / HSQL database
  */
+@Slf4j
 @Configuration
 public class DatabaseConfig {
 
@@ -18,6 +20,7 @@ public class DatabaseConfig {
 
     @PostConstruct
     public void initDB(){
+        log.info("====> DB Init");
         template.execute("CREATE TABLE IF NOT EXISTS users (\n" +
                 "  id         INTEGER PRIMARY KEY,\n" +
                 "  name VARCHAR(30),\n" +
