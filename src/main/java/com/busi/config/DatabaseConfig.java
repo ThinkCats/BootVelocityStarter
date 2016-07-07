a9205664116ad05e5bd39fc36a1540d8780e86fb
wanglei <wanglei@souche.com>
2016-07-07 09:38:15 +0800

    change init config

--- src/main/java/com/busi/config/DatabaseConfig.java
+++ src/main/java/com/busi/config/DatabaseConfig.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 
@@ -12,7 +13,7 @@ import javax.annotation.PostConstruct;
  * For H2 / HSQL database
  */
 @Slf4j
-@Configuration
+@Component
 public class DatabaseConfig {
 
     @Autowired
